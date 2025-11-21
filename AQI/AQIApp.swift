c14bde80f8f4c7ae4e0eded0cc48d87296a6cd3e
Suhas <suhas.gavas@gmail.com>
2025-11-22 00:27:35 +0530

    Initial Commit

---
+++ AQI/AQIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQIApp.swift
+//  AQI
+//
+//  Created by Suhas Gavas on 22/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AQIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AQI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AQI
+//
+//  Created by Suhas Gavas on 22/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
